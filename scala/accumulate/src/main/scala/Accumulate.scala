507cc2083dcfe631e9160c624d94d99ad9a3151d
hsmak <husain.alkhamis@gmail.com>
2019-11-16 13:20:31 -0600

    committing changes..

---
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -0,0 +1,21 @@
+import scala.annotation.tailrec
+
+class Accumulate {
+  def accumulate[A, B](f: (A) => B, list: List[A]): List[B] = list match {
+    case Nil => List[B]()
+    case x :: tail => f(x) :: accumulate(f, tail)
+  }
+
+  // tailrec version
+  def accumulate2[A, B](f: (A) => B, list: List[A]): List[B] = {
+
+    @tailrec
+    def loop[A, B](f: (A) => B, list: List[A], acc: List[B]): List[B] =
+      list match {
+        case Nil => acc
+        case x :: tail => loop(f, tail, acc appended f(x))
+      }
+
+    loop(f, list, List[B]())
+  }
+}
