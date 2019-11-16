bd67dde18f4308239836db51d41d91c3e3350162
hsmak <husain.alkhamis@gmail.com>
2019-11-16 13:37:34 -0600

    committing changes..

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -13,7 +13,7 @@ class Accumulate {
     def loop[A, B](f: (A) => B, list: List[A], acc: List[B]): List[B] =
       list match {
         case Nil => acc
-        case x :: tail => loop(f, tail, acc appended f(x))
+        case x :: tail => loop(f, tail, acc :+ f(x))
       }
 
     loop(f, list, List[B]())
