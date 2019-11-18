50191c4aabde0f15d566720f951e013c2525281c
hsmak <husain.alkhamis@gmail.com>
2019-11-18 13:16:38 -0600

    committing changes..

--- scala/flatten-array/src/main/scala/FlattenArray.scala
+++ scala/flatten-array/src/main/scala/FlattenArray.scala
@@ -1,10 +1,29 @@
+import scala.annotation.tailrec
+
 object FlattenArray {
-  //ToDo - Implement a TailRec solution
+
   def flatten(arr: List[_]): List[_] = {
     arr match {
       case Nil => Nil
       case (x: List[_]) :: tail => flatten(x) ::: flatten(tail)
       case x :: tail => if (x != null) x :: flatten(tail) else flatten(tail)
     }
+  }
+
+  // a TailRec version
+  def flatten2(arr: List[_]): List[_] = {
+    @tailrec
+    def loop(leadArr: List[_], trailArr: List[_], flatAccumulator: List[_]): List[_] = {
+      (leadArr, trailArr) match {
+        case (Nil, Nil) => flatAccumulator
+        case ((x: List[_]) :: tail, _) => loop(x, tail ::: trailArr, flatAccumulator)
+        case (x :: tail, _) =>
+          if (x != null) loop(Nil, tail ::: trailArr, flatAccumulator :+ x)
+          else loop(Nil, tail ::: trailArr, flatAccumulator)
+        case (Nil, _) => loop(trailArr, Nil, flatAccumulator)
+      }
+    }
+
+    loop(arr, Nil, Nil)
   }
 }
