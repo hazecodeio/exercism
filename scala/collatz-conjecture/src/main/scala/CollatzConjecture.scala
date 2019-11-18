cbccadcf1ad8b778ec784e20f91cfc50ff170e90
hsmak <husain.alkhamis@gmail.com>
2019-11-17 22:33:55 -0600

    committing changes..

--- scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -4,7 +4,7 @@ object CollatzConjecture {
 
   def odd(n: Int) = !even(n)
 
-  def steps2(n: Int): Option[Int] = {
+  def steps(n: Int): Option[Int] = {
     def loop(n: Int, acc: Int): Option[Int] = n match {
       case 1 => Some(acc)
       case n if n <= 0 => None
@@ -19,7 +19,7 @@ object CollatzConjecture {
 
   def nextOp(n: Int): Int = if (even(n)) n / 2 else n * 3 + 1
 
-  def steps(n: Int): Option[Int] = n match {
+  def steps2(n: Int): Option[Int] = n match {
     case n if n > 0 => Some(LazyList.iterate(n)(nextOp).takeWhile(_ != 1).length)
     case _ => None
   }
