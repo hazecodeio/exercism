601b07ddf934276500289dcf8886ffdbbd660d03
hsmak <husain.alkhamis@gmail.com>
2019-11-22 13:04:36 -0600

    committing changes..

---
+++ scala/clock/src/main/scala/Clock.scala
@@ -0,0 +1,44 @@
+case class Clock(hour: Int, minute: Int) {
+
+  // Though normalizing beforehand relives us from overriding equal() but it changes the original values
+  //  normalizeClock
+
+  def this(minute: Int) = this(minute / 60, minute % 60)
+
+  private def normalizeClock = {
+    var m = minute
+    var h = hour
+    if (m < 0) {
+      h -= 1 - m / 60
+      m = 60 + (m % 60)
+    }
+    if (h < 0)
+      h = 24 + (h % 24)
+
+    h = (h + m / 60) % 24 // normalize to 24 hours
+    m = m % 60 // normalize to 60 minutes
+
+    Clock(h, m)
+  }
+
+
+  def +(that: Clock) = Clock(this.hour + that.hour, this.minute + that.minute) // Alternatively we can use copy()
+
+  def -(that: Clock) = Clock(this.hour - that.hour, this.minute - that.minute) // Alternatively we can use copy()
+
+  override def equals(that: Any): Boolean = {
+    if (that == null || !that.isInstanceOf[Clock])
+      return false
+
+    val thatNormalized = that.asInstanceOf[Clock].normalizeClock
+    val thisNormalized = this.normalizeClock
+
+    thatNormalized.normalizeClock.hour == thisNormalized.normalizeClock.hour &&
+      thatNormalized.normalizeClock.minute == thisNormalized.normalizeClock.minute
+  }
+
+}
+
+object Clock {
+  def apply(minute: Int) = new Clock(minute)
+}
