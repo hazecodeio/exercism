5a0218e29654a051dbd1cc8e317196ea22dfb820
hsmak <husain.alkhamis@gmail.com>
2019-12-04 12:02:28 -0600

    committing changes..

--- scala/house/src/main/scala/House.scala
+++ scala/house/src/main/scala/House.scala
@@ -1,21 +1,38 @@
+import scala.annotation.tailrec
+
 object House {
 
-  val nouns = "house Jack malt rat cat dog cow horn"
+  val clauses = List(
+    "the house" -> "Jack built.",
+    "the malt" -> "lay in ",
+    "the rat" -> "ate ",
+    "the cat" -> "killed ",
+    "the dog" -> "worried ",
+    "the cow with the crumpled horn" -> "tossed ",
+    "the maiden all forlorn" -> "milked ",
+    "the man all tattered and torn" -> "kissed ",
+    "the priest all shaven and shorn" -> "married ",
+    "the rooster that crowed in the morn" -> "woke ",
+    "the farmer sowing his corn" -> "kept ",
+    "the horse and the hound and the horn" -> "belonged to ")
 
-  val s = """the house that Jack built.
-the malt that lay in the house that Jack built.
-This is the rat that ate the malt that lay in the house that Jack built.
-This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+  def recite(clauseStart: Int, rhymeCount: Int): String = {
 
-"""
-  def recite(clauseStart: Int, rhymeCount: Int) = ???
+    @tailrec
+    def loop(clauseStart: Int, rhymeCount: Int, str: String): String = {
+      (clauseStart == rhymeCount) match {
+        case (true) => str.appendedAll(recite(clauses.slice(0, clauseStart).reverse, "This is ")).appendedAll("\n\n")
+        case _ => loop(clauseStart + 1, rhymeCount, str.appendedAll(recite(clauses.slice(0, clauseStart).reverse, "This is ")).appended('\n'))
+      }
+    }
+
+    loop(clauseStart, rhymeCount, "")
+  }
+
+  private def recite(clauses: List[(String, String)], str: => String): String =
+    clauses match {
+      case Nil => str
+      case head :: tail => recite(tail, str.appendedAll(head._1).appendedAll(" that ").appendedAll(head._2))
+    }
 
 }
