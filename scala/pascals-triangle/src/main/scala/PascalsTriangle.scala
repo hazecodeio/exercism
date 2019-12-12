6158f7f070dad4b3bd2ea23041cf1807aed3b67d
hsmak <husain.alkhamis@gmail.com>
2019-12-12 14:18:56 -0600

    committing changes..

--- scala/pascals-triangle/src/main/scala/PascalsTriangle.scala
+++ scala/pascals-triangle/src/main/scala/PascalsTriangle.scala
@@ -3,6 +3,13 @@ object PascalsTriangle {
   val nextRow = (currentRow: List[Int]) => currentRow.head +: currentRow.sliding(2).map(_.sum).toList :+ currentRow.last
 
   def rows(r: Int): List[List[Int]] = {
+    viaRecursion(r)
+//    viaListIteratorWithSliding(r)
+//    viaStreamIteratorWithSliding(r)
+//    viaStreamIteratorWithZipping(r)
+  }
+
+  private def viaRecursion(r: Int): List[List[Int]] = {
 
     if (r <= 0) return Nil
 
@@ -14,6 +21,30 @@ object PascalsTriangle {
     }
 
     loop(r, 0, Nil)
+  }
+
+  private def viaListIteratorWithSliding(r: Int): List[List[Int]] = {
+
+    if (r <= 0) return Nil
+
+    //Alternatively -> LazyList.iterate()
+    List.iterate(List(1), r) { xs =>
+      (0 +: xs :+ 0).sliding(2).map(_.sum).toList
+    }
+  }
+
+  private def viaStreamIteratorWithSliding(r: Int): List[List[Int]] = {
+    LazyList.iterate(List(1)) { xs => // you also can pass in 'r' as a second parameter similar to List.iterate()
+      (0 +: xs :+ 0).sliding(2).map(_.sum).toList
+    }
+      .take(r).toList
+  }
+
+  private def viaStreamIteratorWithZipping(r: Int): List[List[Int]] = {
+    LazyList.iterate(List(1)) { xs => // you also can pass in 'r' as a second parameter similar to List.iterate()
+      (0 +: xs) zip (xs :+ 0) map { case (l, r) => l + r } // same logic can be used in "viaListIteratorWithSliding()"
+    }
+      .take(r).toList
   }
 
 }
