24f87581f61e83a0f99e374b7b402f3f38a9373f
hsmak <husain.alkhamis@gmail.com>
2020-10-28 15:03:37 -0500

    committing changes (java)...

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,37 @@
+import java.util.function.Function;
+
+enum StrategyE implements Function<String, String> {
+    DEFAULT {
+        @Override
+        public String apply(String s) {
+            char[] chars = s.toCharArray();
+            for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+            }
+            return String.valueOf(chars);
+        }
+    },
+    SB_REVERSE {
+        @Override
+        public String apply(String s) {
+            return new StringBuilder(s).reverse().toString();
+        }
+    }
+}
+
+class ReverseString {
+
+    StrategyE strategy;
+
+    ReverseString() {
+        strategy = StrategyE.DEFAULT;
+    }
+
+    String reverse(String inputString) {
+        return strategy.apply(inputString);
+    }
+
+
+}
