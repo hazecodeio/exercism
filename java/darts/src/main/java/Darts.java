3b94bc466bb522f54b55868ec4a8b895bedb5480
hsmak <husain.alkhamis@gmail.com>
2020-10-29 01:31:43 -0500

    committing changes (java)...

---
+++ java/darts/src/main/java/Darts.java
@@ -0,0 +1,16 @@
+class Darts {
+
+    private double radius;
+
+    Darts(double x, double y) {
+        radius = Math.sqrt(x * x + y * y);
+    }
+
+    int score() {
+        if (radius <= 1) return 10;
+        if (radius <= 5) return 5;
+        if (radius <= 10) return 1;
+        return 0;
+    }
+
+}
