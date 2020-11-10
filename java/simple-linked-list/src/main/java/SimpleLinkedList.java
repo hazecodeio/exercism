5e433c18b2272a0ccec71d018869c815fb839a60
hsmak <husain.alkhamis@gmail.com>
2020-11-10 02:14:17 -0600

    committing changes (java)...

--- java/simple-linked-list/src/main/java/SimpleLinkedList.java
+++ java/simple-linked-list/src/main/java/SimpleLinkedList.java
@@ -1,16 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.NoSuchElementException;
 
-/*
-
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
 public class SimpleLinkedList<E> {
 
     private int size;
