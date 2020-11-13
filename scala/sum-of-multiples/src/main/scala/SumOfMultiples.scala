10c69e091f8acaedd276d6a28a6914c52f03e9f4
hsmak <husain.alkhamis@gmail.com>
2020-11-12 19:41:58 -0600

    committing changes (scala)..

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,9 +1,12 @@
 object SumOfMultiples {
 
-  def multiples(n: Int, max: Int) =
-    (n until max by n).toSet
+  def multiples2(n: Int, max: Int) =
+    LazyList.from(1).map(_ * n).takeWhile(_ < max).toSet
 
   def sum(factors: Set[Int], limit: Int): Int =
     factors.foldLeft(Set[Int]())((acc, n) => acc union multiples(n, limit)).sum
+
+  def multiples(n: Int, max: Int) =
+    (n until max by n).toSet
 }
 
