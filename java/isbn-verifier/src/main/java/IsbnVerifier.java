07a71e980213d30deb941261f894ec86f0094798
hsmak <husain.alkhamis@gmail.com>
2020-11-18 00:16:28 -0600

    committing changes (java)...

---
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -0,0 +1,23 @@
+import java.util.stream.Stream;
+
+class IsbnVerifier {
+
+    boolean isValid(String stringToVerify) {
+
+        String cleanedStr = stringToVerify.replaceAll("-", "");
+
+        if (!cleanedStr.matches("^[0-9]{9}[X0-9]"))
+            return false;
+
+        char[] digits = cleanedStr.toCharArray();
+        int checkDigit = (digits[9] == 'X') ? 10 : Integer.parseInt(String.valueOf(digits[9]));
+
+        int sumOfMultiples = 0;
+        int[] factors = Stream.iterate(10, i -> i - 1).mapToInt(i -> i).limit(10).toArray();
+
+        for (int i = 0; i < digits.length - 1; i++)
+            sumOfMultiples += Integer.parseInt(String.valueOf(digits[i])) * factors[i];
+
+        return (sumOfMultiples + checkDigit) % 11 == 0;
+    }
+}
