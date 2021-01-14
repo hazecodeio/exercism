e24a6a6a674453220682718b5cda48702a2bd416
hsmak <husain.alkhamis@gmail.com>
2021-01-14 16:47:06 -0600

    committing changes (java)...

--- java/list-ops/src/main/java/ListOps.java
+++ java/list-ops/src/main/java/ListOps.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -6,6 +7,11 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * Observations:
+ *      - Maintain the abstract type List<T> as much as possible
+ *      - Not in favor of creating an ArrayList(). What if List was actually a LinkedList
+ */
 class ListOps {
 
     private ListOps() {
@@ -13,30 +19,62 @@ class ListOps {
     }
 
     static <T> List<T> append(List<T> list1, List<T> list2) {
-        return Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+        if(list1.isEmpty())
+            return list2;
+        /*for(T t : list2)
+            list1.add(t);*/ // Arrays.asList() supplies Unmodifiable List in TestCase
+        List<T> appendedList = new ArrayList<>(list1); // not in favor of creating an ArrayList(). What if List was actually a LinkedList
+        appendedList.addAll(list2);
+        return appendedList;
+//        return Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList()); // via Streams
     }
 
     static <T> List<T> concat(List<List<T>> listOfLists) {
-        return listOfLists.stream().flatMap(l -> l.stream()).collect(Collectors.toList());
+        List<T> flattenedList = new ArrayList<>();// not in favor of creating an ArrayList(). What if List was actually a LinkedList
+        for(List<T> l : listOfLists)
+            flattenedList.addAll(l);
+
+        return flattenedList;
+//        return listOfLists.stream().flatMap(l -> l.stream()).collect(Collectors.toList());
     }
 
     static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
-        return list.stream().filter(predicate::test).collect(Collectors.toList());
+
+        List<T> filteredList = new ArrayList<>();
+        for(T t : list)
+            if(predicate.test(t))
+                filteredList.add(t);
+
+        return filteredList;
+//        return list.stream().filter(predicate::test).collect(Collectors.toList());
     }
 
     static <T> int size(List<T> list) {
-        return list.size();
+        int size = 0;
+        for(T t : list)
+            size++;
+
+        return size;
+//        return list.size();
     }
 
     static <T, U> List<U> map(List<T> list, Function<T, U> transform) {
-        return list.stream().map(transform::apply).collect(Collectors.toList());
+        List<U> mappedList = new ArrayList<>();
+        for(T t : list)
+            mappedList.add(transform.apply(t));
+        return mappedList;
+//        return list.stream().map(transform::apply).collect(Collectors.toList());
     }
 
     static <T> List<T> reverse(List<T> list) {
-        return IntStream.range(0, list.size())
+        List<T> reversedList = new ArrayList<>();
+        for(int i = list.size()-1; i >= 0; i--)
+            reversedList.add(list.get(i));
+        return reversedList;
+        /*return IntStream.range(0, list.size())
                 .map(i -> list.size() - 1 - i)
                 .mapToObj(i -> list.get(i))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList());*/
     }
 
     static <T, U> U foldLeft(List<T> list, U initial, BiFunction<U, T, U> f) {
