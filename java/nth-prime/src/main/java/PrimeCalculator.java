75862ff6ed490e30dc054a9e3308bc0bc7488e60
hsmak <husain.alkhamis@gmail.com>
2021-03-08 09:51:22 -0600

    committing changes (java)...

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -6,10 +6,18 @@ class PrimeCalculator {
         if (nth <= 0)
             throw new IllegalArgumentException();
 
-        return IntStream.iterate(2, i -> i + 1).filter(this::isPrime).skip(nth - 1).findFirst().getAsInt();
+        return IntStream.iterate(2, i -> i + 1).filter(this::isPrimeDeclarative).skip(nth - 1).findFirst().getAsInt();
     }
 
-    boolean isPrime(int x) {
+    boolean isPrimeFunctional(int x) {
         return IntStream.rangeClosed(2, (int) Math.sqrt(x)).dropWhile(i -> x % i != 0).findFirst().isEmpty();
+    }
+
+    boolean isPrimeDeclarative(int x){
+        for(int i = 2; i <= (int) Math.sqrt(x); i++){
+            if(x % i == 0)
+                return false;
+        }
+        return true;
     }
 }
