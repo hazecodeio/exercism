ca5c29a9fa5836b6f97345fef7367038069c7224
hsmak <husain.alkhamis@gmail.com>
2021-03-31 16:56:53 -0500

    committing changes (java)...

---
+++ java/minesweeper/src/main/java/MinesweeperBoard.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class MinesweeperBoard {
+
+    List<String> inputBoard;
+    public MinesweeperBoard(List<String> inputBoard) {
+        this.inputBoard = inputBoard;
+    }
+
+    public List<String> withNumbers() {
+        return null;
+    }
+}
